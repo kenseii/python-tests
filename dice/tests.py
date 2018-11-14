23fe4d2e72b0780a06ded833cfbe613e4ed11f43
Agano oasis <oasisagano@gmail.com>
2018-11-14 13:45:00 +0900

    Added a few extra tests for dice upper, lower, hand membership...

--- dice/tests.py
+++ dice/tests.py
@@ -15,6 +15,32 @@ class DieTests(unittest.TestCase):
         self.assertEqual(self.d6.sides, 6)
         self.assertIn(self.d8.value, range(1, 7))
 
+    # test to check if i can add dice instances together and get an integer
+
+    def test_add(self):
+        self.assertIsInstance(self.d6 + self.d8, int)
+
+
+class RollTests(unittest.TestCase):
+    def setUp(self):
+        self.hand1 = dice.Roll('1d2')
+        # 3 dices in hand
+        self.hand3 = dice.Roll('3d6')
+
+    # test the lowest possible output from 3 dices
+    def test_lower(self):
+        self.assertGreaterEqual(int(self.hand3), 3)
+
+    # test the highest possible output from 3, 6 sided dices
+    def test_upper(self):
+        self.assertLessEqual(int(self.hand3), 18)
+
+    def test_membership(self):
+        test_die = dice.Die(2)
+        # give this test die the value of what was in hand1
+        test_die.value = self.hand1.results[0].value
+        self.assertIn(test_die, self.hand1.results)
+
 
 if __name__ == "__main__":
     unittest.main()
