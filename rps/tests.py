8a68839d80509931330b8ea2b0a12e6a5e51ad43
Agano oasis <oasisagano@gmail.com>
2018-11-14 09:06:08 +0900

    added assert equals and not equals tests

--- rps/tests.py
+++ rps/tests.py
@@ -2,12 +2,28 @@ import unittest
 
 import moves
 
+# Inherit testcase class from unittest modules
+
+
 class MoveTests(unittest.TestCase):
     def test_five_plus_five(self):
-        assert 5+5 ==10
+        assert 5+5 == 10
 
     def test_one_plus_one(self):
         assert not 1+1 == 3
+
+# test whether these 2 variables are equal
+    def test_equal(self):
+        rock1 = moves.Rock()
+        rock2 = moves.Rock()
+        self.assertEqual(rock1, rock2)
+
+# test whether these 2 variables are not equal
+    def test_not_equal(self):
+        rock = moves.Rock()
+        paper = moves.Paper()
+        self.assertNotEquals(rock,paper)
+
 
 if __name__ =='__main__':
     unittest.main()
